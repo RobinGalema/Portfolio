e06066d06c2f8027e9763f5974bd4ef62efa855b
Robin <Robin99g@gmail.com>
2019-04-03 13:18:10 +0200

    Added animation for the navbar

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
+
+// Fucntion to make the navbar sticky, activated when the users scrolls on the page
 window.onscroll = function() {scrollbar()};
 
+// Get the navbar from the html page
 var navbar = document.getElementById("navbar");
 
+// Get the location of the top of the navbar
 var sticky = navbar.offsetTop;
 
+// Function to add the sticky class to the navbar if the user scrolls down
 function scrollbar() {
+  // Check the distance between the top of the page and the top of the navbar to check if
+  // there was scrolled
   if (window.pageYOffset >= sticky+20) {
+    // if the distance is more then 20 pixels, add the sticky class to the navbar
     navbar.classList.add("sticky");
   }
   else {
+    // Else remove the sticky class from the navbar
     navbar.classList.remove("sticky");
   }
 }
