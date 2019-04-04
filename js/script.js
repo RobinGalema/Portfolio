447ace0632c2937e2a4398cebb25151ec0e19833
Robin <Robin99g@gmail.com>
2019-04-04 13:32:47 +0200

    started working with ajax and json to load content

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+window.onload = function fetchJSONFile(path, callback) {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function() {
+          if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+              var data = JSON.parse(httpRequest.responseText);
+              if (callback) callback(data);
+            }
+          }
+        };
+        httpRequest.open('GET', path);
+        httpRequest.send();
+      }
+
+      // this requests the file and executes a callback with the parsed result once
+      //   it is available
+      function getData() {
+        fetchJSONFile('content/index.json', function(data) {
+          // do something with your data
+          document.getElementById('body').innerHTML = content
+      });
+    };
+
 
 // Fucntion to make the navbar sticky, activated when the users scrolls on the page
 window.onscroll = function() {scrollbar()};
