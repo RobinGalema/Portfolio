461eecb76ffe9071d5bd99a84fbd9d20ddba5579
Robin <Robin99g@gmail.com>
2019-04-08 14:11:48 +0200

    Worked on Dev page
    Added container and first lay-out of the poc section of the dev page

--- js/script.js
+++ js/script.js
@@ -19,9 +19,10 @@ var currentPage;
       // When the page is loaded, load the index page as the defautt
       window.onload = function getData() {
         fetchJSONFile('content/content.json', function(data) {
-          // do something with your data
 
+          // Load the index page or the last page visited before reloading the page
           getContent(localStorage.getItem("currentPageNumber"));
+          // debug
           console.log(currentPage);
 
           //console.log(data.contentIndex);
@@ -31,7 +32,7 @@ var currentPage;
       });
     };
 
-    // Function to set the content of the page based on the ID of the pageIndex
+    // Function to set the content of the page based on the pageIndex
     // 0 = index / 1 = ptm / 2 = uex / 3 = dev / 4 = sco
     function getContent(pageIndex) {
 
@@ -42,6 +43,7 @@ var currentPage;
           console.log(data.contentIndex);
           document.getElementById('content').innerHTML = data.contentIndex;
           document.getElementById('pageHead').innerHTML = data.indexHead;
+          // Store the index of the page which can be loaded after reloading the page
           currentPage = 0;
           localStorage.setItem("currentPageNumber", currentPage)
         }
@@ -50,6 +52,7 @@ var currentPage;
           console.log(data.contentPTM);
           document.getElementById('content').innerHTML = data.contentPTM;
           document.getElementById('pageHead').innerHTML = data.ptmHead;
+          // Store the index of the page which can be loaded after reloading the page
           currentPage = 1;
           localStorage.setItem("currentPageNumber", currentPage)
         }
@@ -58,6 +61,7 @@ var currentPage;
           console.log(data.contentUEX);
           document.getElementById('content').innerHTML = data.contentUEX;
           document.getElementById('pageHead').innerHTML = data.uexHead;
+          // Store the index of the page which can be loaded after reloading the page
           currentPage = 2;
           localStorage.setItem("currentPageNumber", currentPage)
         }
@@ -66,6 +70,7 @@ var currentPage;
           console.log(data.contentDEV);
           document.getElementById('content').innerHTML = data.contentDEV;
           document.getElementById('pageHead').innerHTML = data.devHead;
+          // Store the index of the page which can be loaded after reloading the page
           currentPage = 3;
           localStorage.setItem("currentPageNumber", currentPage)
         }
@@ -74,6 +79,7 @@ var currentPage;
           console.log(data.contentSCO);
           document.getElementById('content').innerHTML = data.contentSCO;
           document.getElementById('pageHead').innerHTML = data.scoHead;
+          // Store the index of the page which can be loaded after reloading the page
           currentPage = 4
           localStorage.setItem("currentPageNumber", currentPage)
         }
